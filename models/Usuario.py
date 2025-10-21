752466fb9457fecfe7c2912d55e20ea85e8d9e5d
Emmanuel <alvarezemmanuel0203@gmail.com>
2025-10-21 12:54:33 -0500

    Añadido el rol del usuario

--- models/Usuario.py
+++ models/Usuario.py
@@ -13,11 +13,17 @@ class Usuario(UserMixin):
     @classmethod
     def from_dict(cls, data):
         return cls(
-            idusuario=data.get("idusuario"),
+            idusuario=data.get("idUsuario"),  # Corregido para usar "idUsuario" en lugar de "idusuario"
             nombre=data.get("nombre"),
             apellido=data.get("apellido"),
             correo=data.get("correo"),
             telefono=data.get("telefono"),
-            fkidrol=data.get("fkidrol"),
+            fkidrol=data.get("fkidRol"),
             password=data.get("password")
         )
+
+    @property
+    def is_admin(self):
+        # Compara el fkidrol con el ID del rol de administrador
+        is_admin = self.fkidrol == "1dd96a79-3a98-409d-b875-8585aca8315a"
+        return is_admin
